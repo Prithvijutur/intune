d1383ac9e816530f7f5352184b1a6abdfa1e0a4e
= <pjutur@box.com>
2021-08-19 14:42:15 -0700

    sample project to build with intune on xcode 13

---
+++ intune/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  intune
+//
+//  Created by Prithvi Jutur on 8/18/21.
+//
+
+import UIKit
+import IntuneMAMSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
